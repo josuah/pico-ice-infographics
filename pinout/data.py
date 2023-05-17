391a3bc5f1de76f2ebd30c26af9350f0605b979a
Josuah Demangeon <me@josuah.net>
2023-05-17 08:13:55 +0200

    initial commit

---
+++ pinout/data.py
@@ -0,0 +1,227 @@
+legend = [
+    ("Analog", "analog"),
+    ("Communication", "comms"),
+    ("Ground", "gnd"),
+    ("GPIO", "gpio"),
+    ("Touch", "touch"),
+    ("Power", "pwr"),
+    ("PWM", "pwm"),
+]
+
+# Pinlabels
+
+left_header = [
+    [
+        ("0", "gpio"),
+        ("A0", "analog"),
+        ("MISO", "comms"),
+    ],
+    [
+        ("1", "gpio"),
+        ("MOSI", "comms", {"body": {"x": 92}}),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+        ("SCLK", "comms"),
+    ],
+]
+
+right_header = [
+    [
+        ("0", "gpio"),
+        ("A0", "analog"),
+        ("MISO", "comms"),
+    ],
+    [
+        ("1", "gpio"),
+        ("MOSI", "comms", {"body": {"x": 92}}),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+        ("SCLK", "comms"),
+    ],
+]
+
+
+# Text
+
+title = "<tspan class='h1'>pico-ice</tspan>"
+
+description = """A small, low cost board featuring:
+- a Raspberry Pi Pico processor (RP2040)
+- a Lattice Semiconductor iCE40UP5K FPGA
+<tspan class='italic strong'>pico-ice.tinyvision.ai</tspan>"""
